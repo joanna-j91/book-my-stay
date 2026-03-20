e02addac4886d6de18c4770bdce8c9e96b39ef64
Joanna Jacob <joannajacob910@gmail.com>
2026-03-20 14:07:40 +0530

    finished use case 4

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,86 @@
 //Author : Joanna Jacob
-//version : 3.0
+//version : 4.0
 
 import java.util.HashMap;
 
-class RoomInventory {
+abstract class Room4 {
+
+    String type;
+    double price;
+
+    Room4(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println(type + " Price: " + price);
+    }
+}
+
+class SingleRoom4 extends Room4 {
+    SingleRoom4() {
+        super("Single", 1000);
+    }
+}
+
+class DoubleRoom4 extends Room4 {
+    DoubleRoom4() {
+        super("Double", 2000);
+    }
+}
+
+class SuiteRoom4 extends Room4 {
+    SuiteRoom4() {
+        super("Suite", 5000);
+    }
+}
+
+class RoomInventory4 {
 
     HashMap<String, Integer> inventory;
 
-    RoomInventory() {
+    RoomInventory4() {
         inventory = new HashMap<>();
 
         inventory.put("Single", 5);
-        inventory.put("Double", 3);
+        inventory.put("Double", 0);
         inventory.put("Suite", 2);
     }
 
     int getAvailability(String type) {
         return inventory.get(type);
     }
-
-    void updateAvailability(String type, int count) {
-        inventory.put(type, count);
-    }
-
-    void displayInventory() {
-        for (String key : inventory.keySet()) {
-            System.out.println(key + " Available: " + inventory.get(key));
-        }
-    }
 }
 
 public class Main {
 
-    static String version = "Hotel Booking System v3.1";
+    static String version = "Hotel Booking System v4.0";
 
     public static void main(String[] args) {
 
         System.out.println("Book My Stay");
         System.out.println(version);
 
-        RoomInventory inv = new RoomInventory();
+        RoomInventory4 inv = new RoomInventory4();
 
-        inv.displayInventory();
+        Room4 r1 = new SingleRoom4();
+        Room4 r2 = new DoubleRoom4();
+        Room4 r3 = new SuiteRoom4();
 
-        inv.updateAvailability("Single", 4);
+        if (inv.getAvailability("Single") > 0) {
+            r1.display();
+            System.out.println("Available: " + inv.getAvailability("Single"));
+        }
 
-        System.out.println("After Update");
+        if (inv.getAvailability("Double") > 0) {
+            r2.display();
+            System.out.println("Available: " + inv.getAvailability("Double"));
+        }
 
-        inv.displayInventory();
+        if (inv.getAvailability("Suite") > 0) {
+            r3.display();
+            System.out.println("Available: " + inv.getAvailability("Suite"));
+        }
     }
 }
